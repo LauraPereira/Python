fe1588490e3f10825f778487d0d4ce648d819bca
Charly PRAUD <charly.praud@etu.univ-nantes.fr>
2020-01-17 13:12:20 +0100

    AgentV2

--- agentfinal.py
+++ agentfinal.py
@@ -1,80 +1,93 @@
-import requests, json, psutil, platform,time;
+import requests, json, psutil, platform,time,os,sys
+
 url="http://192.168.3.26:5000/api"
 
-while(1):
-
-    #Récupération load average 1min
-    un, cinq, quinze = psutil.getloadavg()
-    print(un)
-
-    #Récupération de l'OS
-    systeme = platform.system()
+a = [p.name() for p in psutil.process_iter(attrs=['name'])]
+if 'az' in a :
+    print ("gg")
     
-    #Récupération de l'hote
-    nomHost = platform.node()
-    
-    #Récupération de noyau
-    version = platform.release()
-    
-    #Récupération du type cpu
-    cpu = platform.processor()
-    
-    #Récupération de la fréquence cpu
-    cpuFre = psutil.cpu_freq().current
-    
-    # calculate the uptime
-    uptime_file = open('/proc/uptime')
-    uptime = uptime_file.readline().split()[0]
-    uptime_file.close()
-    uptime = float(uptime)
-    (uptime,secs) = (int(uptime / 60), uptime % 60)
-    (uptime,mins) = divmod(uptime,60)
-    (days,hours) = divmod(uptime,24)
-    uptime = 'up %d jour%s, %d:%02d' % (days, days != 1 and 's' or '', hours, mins)
+try:
+    with open('/home/rt/test/test.txt','r'): 
+        while(1):
+            #Récupération de l'OS
+            systeme = platform.system()
 
-    tabDisk = []
-    listdisk = psutil.disk_partitions()
-    for disk in listdisk:
-        if (disk.fstype != 'squashfs'):
-            detaildisk = psutil.disk_usage(disk.mountpoint)
-            tabDisk.append({'fileSystem':disk.device,'size':detaildisk.total,'used':detaildisk.used,'available':detaildisk.free,'pourcentage':detaildisk.percent,'mounted':disk.mountpoint})
-    
-    with open('/home/rt/test/test.txt','r')as f:
-        service=f.read()
-        service=json.loads(service)
-    
-    #Mémoire utilisé
-    memoireused = psutil.virtual_memory().used
+            #Récupération de l'hote
+            nomHost = platform.node()+"@192.168.2.25"
 
-    #Mémoire free
-    memoirefree = psutil.virtual_memory().free
+            #Récupération de noyau
+            version = platform.release()
+
+            #Récupération du type cpu
+            cpu = platform.processor()
+
+            #Récupération de la fréquence cpu
+            cpuFre = psutil.cpu_freq().current
+            cpuMax = psutil.cpu_freq().max
+
+            # calculate the uptime
+            uptime_file = open('/proc/uptime')
+            uptime = uptime_file.readline().split()[0]
+            uptime_file.close()
+            uptime = float(uptime)
+            (uptime,secs) = (int(uptime / 60), uptime % 60)
+            (uptime,mins) = divmod(uptime,60)
+            (days,hours) = divmod(uptime,24)
+            uptime = 'up %d jour%s, %d:%02d' % (days, days != 1 and 's' or '', hours, mins)
+
+            tabDisk = []
+            listdisk = psutil.disk_partitions()
+            for disk in listdisk:
+                if (disk.fstype != 'squashfs'):
+                    detaildisk = psutil.disk_usage(disk.mountpoint)
+                    tabDisk.append({'fileSystem':disk.device,'size':detaildisk.total,'used':detaildisk.used,'available':detaildisk.free,'pourcentage':detaildisk.percent,'mounted':disk.mountpoint})
+
+            with open('/home/rt/test/test.txt','r')as f:
+                service=f.read()
+                service=json.loads(service)
+
+            #Mémoire utilisé
+            memoireused = psutil.virtual_memory().used
+
+            #Mémoire free
+            memoirefree = psutil.virtual_memory().free
 
 
-    #Mémoire buffers
-    memoirebuffers = psutil.virtual_memory().buffers
+            #Mémoire buffers
+            memoirebuffers = psutil.virtual_memory().buffers
 
 
-    #Mémoire cached
-    memoirecached = psutil.virtual_memory().cached
+            #Mémoire cached
+            memoirecached = psutil.virtual_memory().cached
 
-    service["id"]=1
-    service["os"]= systeme
-    service["nomh"]= nomHost
-    service["noyl"]= version
-    service["cputype"]= cpu
-    service["cpufrequence"]= cpuFre
-    service["up"]=uptime
-    service["metrique"]=tabDisk
-    service["moccupe"]=memoireused
-    service["mlibre"]=memoirefree
-    service["mbuffer"]=memoirebuffers
-    service["mcached"]=memoirecached
-    service["total"]=memoireused+memoirefree+memoirebuffers+memoirecached
-    print(service)
-    
-    donnees = json.dumps(service)
-    try:
-        r = requests.post(url, data=donnees)
-    except:
-        pass
-    time.sleep(60)
+            service["id"]=14603
+            service["os"]= systeme
+            service["nomhost"]= nomHost
+            service["noyau"]= version
+            service["cputype"]= cpu
+            service["cpufrequence"]= cpuFre
+            service["uptime"]=uptime
+            service["metrique"]=tabDisk
+            service["moccupe"]=memoireused
+            service["mlibre"]=memoirefree
+            service["mbuffer"]=memoirebuffers
+            service["mcached"]=memoirecached
+            service["total"]=memoireused+memoirefree+memoirebuffers+memoirecached
+            service["cpufrequencemax"]=cpuMax
+            print(service)
+
+            donnees = json.dumps(service)
+            try:
+                r = requests.post(url, data=donnees)
+            except:
+                pass
+            time.sleep(60)
+except IOError:
+    nom = platform.node()+"@192.168.2.25"
+    urlconfig = "http://192.168.3.26:5000/api/init"
+    r = requests.post(urlconfig,nom)
+    #reponse = requests.get(urlconfig)
+    print(r.json())
+    print("erreur")
+
+
